0527d839b3ae6f8c10f115886c686f9b09dc778d
severikupari1 <severikupari1@gmail.com>
2022-06-30 10:43:21 +0300

    added test annotation

--- core/src/test/java/org/helsinki/paytrail/request/auth/PaytrailAuthHeadersTest.java
+++ core/src/test/java/org/helsinki/paytrail/request/auth/PaytrailAuthHeadersTest.java
@@ -3,11 +3,13 @@ package org.helsinki.paytrail.request.auth;
 import junit.framework.TestCase;
 import org.helsinki.paytrail.request.auth.constants.PaytrailAuthHeaders;
 import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 public class PaytrailAuthHeadersTest extends TestCase {
 
+    @Test
     public void testKeys() {
         List<String> sortedKeys = PaytrailAuthHeaders.getSortedKeys();
         System.out.println(sortedKeys);
