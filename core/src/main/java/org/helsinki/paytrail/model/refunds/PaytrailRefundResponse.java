90546350a18f8925c122f3812681882095a1c4df
Severi Kupari <severikupari1@gmail.com>
2022-11-25 11:22:39 +0200

    Update PaytrailRefundResponse.java

--- core/src/main/java/org/helsinki/paytrail/model/refunds/PaytrailRefundResponse.java
+++ core/src/main/java/org/helsinki/paytrail/model/refunds/PaytrailRefundResponse.java
@@ -9,9 +9,8 @@ import org.helsinki.paytrail.response.PaytrailResponse;
 import java.io.Serializable;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
-public class PaytrailRefundResponse extends PaytrailResponse implements Serializable {
+public class PaytrailRefundResponse implements Serializable {
     private String transactionId;
     private String provider;
     private RefundStatus status;
