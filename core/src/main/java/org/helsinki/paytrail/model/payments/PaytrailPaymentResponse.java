3b464f3cec52182070674c99b11529f93d6b09b7
Severi Kupari <severikupari1@gmail.com>
2022-11-25 11:21:50 +0200

    Update PaytrailPaymentResponse.java

--- core/src/main/java/org/helsinki/paytrail/model/payments/PaytrailPaymentResponse.java
+++ core/src/main/java/org/helsinki/paytrail/model/payments/PaytrailPaymentResponse.java
@@ -2,7 +2,6 @@ package org.helsinki.paytrail.model.payments;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import org.helsinki.paytrail.response.PaytrailResponse;
 
@@ -10,9 +9,8 @@ import java.io.Serializable;
 import java.util.List;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
-public class PaytrailPaymentResponse extends PaytrailResponse implements Serializable {
+public class PaytrailPaymentResponse implements Serializable {
     private String transactionId;
     private String href;
     private String terms;
