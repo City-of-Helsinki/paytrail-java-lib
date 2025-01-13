b47f50d1fb9080cec73bbc7a0eb900cf3af3eb72
Jarkko Laukkanen <jarkko.laukkanen@ambientia.fi>
2025-01-13 16:04:39 +0200

    [KYV-1138] review fixes

--- core/src/main/java/org/helsinki/paytrail/util/LogUtil.java
+++ core/src/main/java/org/helsinki/paytrail/util/LogUtil.java
@@ -7,30 +7,34 @@ public class LogUtil {
     public static void filteredLog(Logger log, String text){
         // filter possible cardtoken out of logs
         text = filterJsonElementFromString(
+                log,
                 text,
                 "token",
                 "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx");
         log.info(text);
     }
 
-    public static String filterJsonElementFromString(String stringToFilter, String elementName, String valueReplace){
-        String elementNameString = "\"" + elementName + "\"";
-        String valueReplaceString = "\"" + valueReplace + "\"";
-        int tokenElementStartIndex = stringToFilter.indexOf(elementNameString);
-        if( tokenElementStartIndex != -1){
-            int tokenEndIndex = stringToFilter.indexOf(",", tokenElementStartIndex);
-            if( tokenEndIndex == -1 )
-            {
-                // was not array or end of an array
-                // search end of an object
-                tokenEndIndex = stringToFilter.indexOf("}", tokenElementStartIndex);
-                if( tokenEndIndex == -1 ){
-                    // could not locate section to filter, return unfiltered
-                    return stringToFilter;
+    public static String filterJsonElementFromString(Logger log, String stringToFilter, String elementName, String valueReplace){
+        try{
+            String elementNameString = "\"" + elementName + "\"";
+            String valueReplaceString = "\"" + valueReplace + "\"";
+            int tokenElementStartIndex = stringToFilter.indexOf(elementNameString);
+            if( tokenElementStartIndex != -1) {
+                int tokenEndIndex = stringToFilter.indexOf(",", tokenElementStartIndex);
+                if (tokenEndIndex == -1) {
+                    // was not array or end of an array
+                    // search end of an object
+                    tokenEndIndex = stringToFilter.indexOf("}", tokenElementStartIndex);
+                    if (tokenEndIndex == -1) {
+                        // could not locate section to filter, return unfiltered
+                        return stringToFilter;
+                    }
                 }
+                String tokenElement = stringToFilter.substring(tokenElementStartIndex, tokenEndIndex);
+                return stringToFilter.replace(tokenElement, elementNameString + ":" + valueReplaceString);
             }
-            String tokenElement = stringToFilter.substring(tokenElementStartIndex,tokenEndIndex);
-            stringToFilter = stringToFilter.replace(tokenElement,elementNameString + ":" + valueReplaceString);
+        } catch (Exception e) {
+            log.error("Error while filtering log.", e);
         }
         return stringToFilter;
     }
